274bda8df4447c63c46a37c0a55370043e6211eb
Martin Nemec <martinnemec3@gmail.com>
2018-11-06 21:40:55 +0100

    Spring Boot 2.1 project created

---
+++ src/main/java/com/github/martinnemec3/openidconnectserver/OpenidConnectServerApplication.java
@@ -0,0 +1,12 @@
+package com.github.martinnemec3.openidconnectserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenidConnectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenidConnectServerApplication.class, args);
+	}
+}
